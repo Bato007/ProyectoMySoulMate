9538a5407e14cc950bce8385d64d089762789d4a
Brandon Hernández <brandonhernandez_10@outlook.es>
2020-05-04 18:30:45 -0600

    Las clases y un txt con la base de datos (falta)

---
+++ src/Clases.py
@@ -0,0 +1,19 @@
+class Artista:
+    
+    def _init_(self, artist_name, songs, similar_artists):
+        self.artist_name = artist_name
+        self.songs = songs[:]
+        self.similar_artists = similar_artists[:]
+    
+    def getInfo():
+        return self.artist_name, "tiene",self.songs.len(), "canciones en la base de datos y", self.similar_artist.len(), "artistas similares"
+            
+class Cancion:
+    
+    def _init_(self, song_name, album, song_genre):
+        self.song_name = song_name
+        self.album = album
+        self.song_genre = song_genre
+        
+    def getInfo():    
+        return "La cancion", self.song_name,"pertenece al album",self.album,"del genero",self.song_genre
