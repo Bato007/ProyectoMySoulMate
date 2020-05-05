d4147072b487ab2284b20f9235a6d89b9c03ddb6
Andrea <andreaa.malin@gmail.com>
2020-05-05 10:58:19 -0600

    La base de datos ya tiene todas sus funciones

--- src/Base_de_Datos.py
+++ src/Base_de_Datos.py
@@ -1,10 +1,13 @@
 import re
+import random
 from Clases import *
+from GrafoInicial import *
+
 
 def realizar_base():
     base = [] #array de array
+    final = []
     actual = [] #Llevara los items de cada artista
-    final = [] #La base final a retornar
     
     with open("Base_de_Datos.txt") as f:
         temporal = f.read()
@@ -12,7 +15,7 @@ def realizar_base():
     
     temp = temporal.split('\n')
     temp2 = []
-    for i in range(int(len(temp)/2)): #Se agarra la mitad para no agregar los array de enters
+    for i in range(int(len(temp))): 
         temp2.append(temp[i].split(';')) #Se separa por ;
 
     for item in temp2: #Por elemento en temporal2
@@ -41,4 +44,55 @@ def realizar_base():
                 actual.append(result) #Se agregan los similares
                 final.append(Artista(actual[0], actual[1], actual[2])) #Se inserta el artista actual a la db final
     
+    
     return final #Se retorna la base
+
+def mostrar_base():
+    final = realizar_base()
+    
+    info_final = []
+    for artista in final: #Por cada artista en la base final
+        info_final.append(artista.getInfo()) #Se obtiene su informacion
+    
+    return info_final #Se retorna
+
+def buscar_artista(nombre_artista):
+    final = realizar_base()
+    
+    for artista in final:
+        if(artista.artist_name == nombre_artista):
+            print('artista ingresado', nombre_artista)
+            print('grafo', generar_grafo(nombre_artista))
+            return generar_grafo(nombre_artista)
+    
+
+def buscar_cancion(grafo):
+    final = realizar_base()
+    
+    seguir = True
+    contador = 1 #Empezamos del mas alejado al artista acual
+    while(seguir): #Se verifica que este en la base de datos
+        posible = grafo[len(grafo)-contador] #Se usa la lista de posibles artistas (posicion del grafo)
+        
+        for artist in final:
+            if artist.artist_name == posible:
+                posible = artist.similar_artists #Lista de posibles artistas (similares al posible artista del grafo)
+                elegido = random.randint(0, len(posible)-1) #Se genera un numero aleatorio entre los artistas posibles
+                artista_actual = posible[elegido] #Se selecciona ese artista
+                seguir = False #Ya no hay necesidad de seguir moviendonos
+        contador+=1
+                
+    
+    print('Similares del elegido del grafo', artista_actual)
+    
+    for item in final:
+        if artista_actual == item.artist_name:
+            elegido = random.randint(0, len(item.songs)-1) #Se genera un numero aleatorio entre las canciones posibles
+                    
+            return item.songs[elegido].getInfo() #Se obtiene una cancion aleatoria
+            
+            
+#Como se debe de usar
+print(mostrar_base()) #Para mostrar la base
+grafo = buscar_artista('Bea Miller') #Para buscar un artista
+print(buscar_cancion(grafo)) #Para encontrar la cancion recomendada
--- src/Clases.py
+++ src/Clases.py
@@ -5,8 +5,8 @@ class Artista:
         self.songs = songs[:]
         self.similar_artists = similar_artists[:]
     
-    def getInfo():
-        return self.artist_name, "tiene",self.songs.len(), "canciones en la base de datos y", self.similar_artist.len(), "artistas similares"
+    def getInfo(self):
+        return self.artist_name, "tiene", len(self.songs), "canciones en la base de datos y", len(self.similar_artists), "artistas similares"
             
 class Cancion:
     
@@ -15,5 +15,5 @@ class Cancion:
         self.album = album
         self.song_genre = song_genre
         
-    def getInfo():    
+    def getInfo(self):    
         return "La cancion", self.song_name,"pertenece al album",self.album,"del genero",self.song_genre
