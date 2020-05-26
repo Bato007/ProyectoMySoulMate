01b627aceca98f383700556555e54afac9cbaa64
lauratamath <laurylevi@hotmail.com>
2020-05-26 00:45:32 -0600

    solo esta la estructura del main

--- Segunda Parte/driver.py
+++ Segunda Parte/driver.py
@@ -7,4 +7,40 @@
 # Nombre: driver.py
 
 #Importando las funciones de la base de datos
-import database as *
+#import database as *
+
+opcionn = True
+ejecucion = True
+validador = True
+
+while ejecucion:
+    if validador:
+        print ('-----------Bienvenido al sistemas de recomendación de música-----------')
+        print ('\t1.Buscar por géneros similares \n\t2.Buscar por artista similar\n\t3.Buscar lo mejor de ambas décadas')
+
+        while opcionn:
+            opcion = input('\tIngrese la opción a ejecutar: ')
+            print ('\n')
+            try:
+                opcion = int(opcion)
+                opcionn = False
+
+            except ValueError:
+                print('Opcion inválida!')
+                
+    opcionn = True
+    
+    #Buscar por artista similar
+    if opcion == 1:
+         print('v')
+
+    #Buscar por artista similar
+    if opcion == 2:
+         print('f')
+    #Buscar lo mejor de ambas decadas
+    if opcion == 3:
+         print('g')
+    #Salir
+    if opcion == 4:
+        print('Gracias por utilizar este sistema de recomendación')
+        ejecucion = False
